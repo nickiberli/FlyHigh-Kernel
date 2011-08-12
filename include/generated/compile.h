c48d4803831572425e0c7cd94b99efcca924a295
Nicki Berli <nickiberli@gmail.com>
2011-08-12 10:43:27 -0700

    recompiled -> wifi working

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Thu Aug 11 11:56:41 PDT 2011"
-#define LINUX_COMPILE_TIME "11:56:41"
+#define UTS_VERSION "#4 PREEMPT Fri Aug 12 10:32:10 PDT 2011"
+#define LINUX_COMPILE_TIME "10:32:10"
 #define LINUX_COMPILE_BY "nicki"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.10-g6d8fc04-dirty"
+#define UTS_RELEASE "2.6.35.10-g975ae7e-dirty"
