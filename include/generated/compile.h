d9b0298e0cccd7473c0a29e1419d0e50e01cd8e8
Nicki Berli <nickiberli@gmail.com>
2011-08-20 23:15:52 +0200

    set brazilianwax to default

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 PREEMPT Mon Aug 15 07:42:31 PDT 2011"
-#define LINUX_COMPILE_TIME "07:42:31"
+#define UTS_VERSION "#10 PREEMPT Thu Aug 18 22:38:29 CEST 2011"
+#define LINUX_COMPILE_TIME "22:38:29"
 #define LINUX_COMPILE_BY "nicki"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "(none)"
