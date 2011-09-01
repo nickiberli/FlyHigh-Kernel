488fbac2c4ad70945675c06df6bc82b472f527c8
Nicki Berli <nickiberli@gmail.com>
2011-09-01 18:23:04 +0200

    FIXED usb mounting bug (thx to virtuous)

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 12 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 PREEMPT Thu Aug 18 22:38:29 CEST 2011"
-#define LINUX_COMPILE_TIME "22:38:29"
+#define UTS_VERSION "#12 PREEMPT Wed Aug 31 21:29:50 CEST 2011"
+#define LINUX_COMPILE_TIME "21:29:50"
 #define LINUX_COMPILE_BY "nicki"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "(none)"
